a9cbd9d1ea265cc7d123ca3499f716a31c0e648d
JavaidAhmadKhan <relaxjavaid@gmail.com>
2021-10-16 14:01:14 +0530

    adding ssr and apis

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,16 @@ export default function Home() {
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Pull some data from server -API endpoints */}
+        </section>
+      </main>
     </div>
   );
 }
